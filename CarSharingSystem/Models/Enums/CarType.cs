d8f439800fce663060da3d4702c9991eb4ec3566
Jakub Kasprzak <kuba.kasprzak12@gmail.com>
2025-06-04 21:32:39 +0200

    Add CarType enum

--- CarSharingSystem/Models/Enums/CarType.cs
+++ CarSharingSystem/Models/Enums/CarType.cs
@@ -2,5 +2,20 @@
 {
     public enum CarType
     {
+        Sedan,
+        Hatchback,
+        StationWagon,
+        SUV,
+        Coupe,
+        Convertible,
+        Minivan,
+        Pickup,
+        Roadster,
+        Van,
+        Crossover,
+        Limousine,
+        Sports,
+        OffRoad,
+        Microcar
     }
 }
