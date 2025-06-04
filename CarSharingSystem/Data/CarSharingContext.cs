f00068399cffc3185661c323dcfad122998a0913
Jakub Kasprzak <kuba.kasprzak12@gmail.com>
2025-06-04 22:22:09 +0200

    Add AppDbContext

---
+++ CarSharingSystem/Data/CarSharingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SqlServer;
+using CarSharingSystem.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSharingSystem.Data
+{
+    public class CarSharingContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Rental> Rental { get; set; }
+        public DbSet<User> users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarSharingDb;Trusted_Connection=True;");
+        }
+    }
+}
