e5caad0681b8706633d053cd53a405bb39fa8197
Jakub Kasprzak <kuba.kasprzak12@gmail.com>
2025-06-08 14:02:27 +0200

    Add get method GetByCarType

--- CarSharingSystem/Controllers/CarsController.cs
+++ CarSharingSystem/Controllers/CarsController.cs
@@ -1,9 +1,11 @@
 ﻿using CarSharingSystem.Data;
+using CarSharingSystem.Models.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarSharingSystem.Controllers
 {
+    [Route("Api/[controller]")]
     [ApiController]
     public class CarsController : Controller
     {
@@ -13,12 +15,48 @@ namespace CarSharingSystem.Controllers
             _context = context;
         }
         [HttpGet]
-        [Route("Cars/GetAll")]
+        [Route("GetAll")]
         public async Task<IActionResult> GetAllCars()
         {
             var Cars = await _context.Cars.ToListAsync();
-
+            if (Cars == null || Cars.Count == 0) {
+                return NotFound("No cars found");
+            }
             return Ok(Cars);
         }
+        [HttpGet]
+        [Route("GetByBrand")]
+        public async Task<IActionResult> GetByBrand(string brand)
+        {
+            var cars = await _context.Cars
+                                     .Where(c => c.Brand.ToLower() == brand.ToLower())
+                                     .ToListAsync();
+            if (!cars.Any())
+            {
+                return NotFound($"No cars found for brand {brand}");
+            }
+            return Ok(cars);
+        }
+        [HttpGet]
+        [Route("GetByType")]
+        public async Task<IActionResult> GetByType(string type)
+        {
+            if (string.IsNullOrWhiteSpace(type))
+            {
+                return BadRequest("Type parameter is required");
+            }
+
+            if (!Enum.TryParse(type, true, out CarType carType))
+            {
+                return BadRequest("Invalid car type");
+            }
+
+            var cars = await _context.Cars
+                .Where(c => c.CarType == carType)
+                .ToListAsync();
+
+            return Ok(cars);
+        }
+
     }
 }
--- CarSharingSystem/Program.cs
+++ CarSharingSystem/Program.cs
@@ -2,11 +2,15 @@ using CarSharingSystem.Data;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 //Add CarSharingContext
 builder.Services.AddDbContext<CarSharingContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
