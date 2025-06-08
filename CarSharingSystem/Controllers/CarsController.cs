97cd84fd7ebe515a672f71309b5520096d119731
Jakub Kasprzak <kuba.kasprzak12@gmail.com>
2025-06-08 13:34:41 +0200

    Add controller GetAllCars

---
+++ CarSharingSystem/Controllers/CarsController.cs
@@ -0,0 +1,24 @@
+﻿using CarSharingSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSharingSystem.Controllers
+{
+    [ApiController]
+    public class CarsController : Controller
+    {
+        private readonly CarSharingContext _context;
+        public CarsController(CarSharingContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [Route("Cars/GetAll")]
+        public async Task<IActionResult> GetAllCars()
+        {
+            var Cars = await _context.Cars.ToListAsync();
+
+            return Ok(Cars);
+        }
+    }
+}
--- CarSharingSystem/Program.cs
+++ CarSharingSystem/Program.cs
@@ -4,7 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 //Add CarSharingContext
 builder.Services.AddDbContext<CarSharingContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
@@ -12,13 +14,22 @@ var app = builder.Build();
 app.MapOpenApi();
 
 // Add Scalar
-app.MapScalarApiReference();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<CarSharingContext>();
     context.Database.EnsureCreated(); // Stworzy bazê i zaaplikuje seed
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
